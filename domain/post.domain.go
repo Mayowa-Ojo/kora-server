6a2ea5b8032bde47550438b0e67f971ecd4866ef
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-18 16:30:18 -0600

    :sparkles: [feat]: define post-{service,repository} interface

--- domain/post.domain.go
+++ domain/post.domain.go
@@ -2,15 +2,18 @@ package domain
 
 import (
 	"github.com/Mayowa-Ojo/kora/entity"
+	"github.com/Mayowa-Ojo/kora/types"
 	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // PostService -
 type PostService interface {
-	GetAll(ctx *fiber.Ctx) ([]entity.Post, error)
+	GetAll(ctx *fiber.Ctx, opts types.GenericMap) ([]types.GenericMap, error)
 }
 
 // PostRepository -
 type PostRepository interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Post, error)
+	GetMany(ctx *fiber.Ctx, filter types.Any, opts *options.FindOptions) ([]entity.Post, error)
 }
