46e1043277e9c03894a8096087c18319f09c958e
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-18 16:24:46 -0600

    :sparkles: [feat]: create entry point for application middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,25 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware"
+	"github.com/gofiber/helmet"
 )
 
 // InitMiddlewares -
 func InitMiddlewares(app *fiber.App) {
+	app.Use(middleware.Logger(middleware.LoggerConfig{
+		Format:     "${time} ${method} ${path}",
+		TimeFormat: "15:04:05",
+		TimeZone:   "Local",
+		Output:     os.Stdout,
+	}))
+
+	app.Use(middleware.Recover())
+
+	app.Use(helmet.New())
+
 	app.Use(NotFoundError)
 }
