84091d9f3d70e3b936ebcc4971ed0fc5ae177826
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-22 15:47:34 -0600

    :sparkles: [feat]: define post-{service,repository} interface methods

--- domain/post.domain.go
+++ domain/post.domain.go
@@ -4,16 +4,27 @@ import (
 	"github.com/Mayowa-Ojo/kora/entity"
 	"github.com/Mayowa-Ojo/kora/types"
 	"github.com/gofiber/fiber"
+	mg "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // PostService -
 type PostService interface {
-	GetAll(ctx *fiber.Ctx, opts types.GenericMap) ([]types.GenericMap, error)
+	GetAll(ctx *fiber.Ctx) ([]entity.Post, error)
+	GetOne(ctx *fiber.Ctx) (*entity.Post, error)
+	// GetMany(ctx *fiber.Ctx) (*entity.Post, error)
+	Create(ctx *fiber.Ctx) (*entity.Post, error)
+	// UpdateOne(ctx *fiber.Ctx) (*entity.Post, error)
+	// UpdateMany(ctx *fiber.Ctx) (*entity.Post, error)
+	// DeleteOne(ctx *fiber.Ctx) (*entity.Post, error)
 }
 
 // PostRepository -
 type PostRepository interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Post, error)
 	GetMany(ctx *fiber.Ctx, filter types.Any, opts *options.FindOptions) ([]entity.Post, error)
+	GetOne(ctx *fiber.Ctx, filter types.Any, opts *options.FindOneOptions) (*entity.Post, error)
+	UpdateOne(ctx *fiber.Ctx, filter types.Any, update types.Any) (*mg.UpdateResult, error)
+	DeleteOne(ctx *fiber.Ctx, filter types.Any) (*mg.DeleteResult, error)
+	Create(ctx *fiber.Ctx, post *entity.Post) (*mg.InsertOneResult, error)
 }
