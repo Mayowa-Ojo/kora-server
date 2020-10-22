bb53cd3b2564802ff296106fe662b4bc41193ba1
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-22 15:42:38 -0600

    :sparkles: [feat]: add <omitempty> struct tag to fields

--- entity/post.entity.go
+++ entity/post.entity.go
@@ -3,41 +3,40 @@ package entity
 import (
 	"time"
 
-	validation "github.com/go-ozzo/ozzo-validation"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Post - defines the schema of a post collection
 type Post struct {
-	ID          primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	Title       string               `json:"title"        bson:"title"`
-	Content     string               `json:"content"      bson:"content"`
-	Upvotes     int                  `json:"upvotes"      bson:"upvotes"`
-	Downvotes   int                  `json:"downvotes"    bson:"downvotes"`
-	Views       int                  `json:"views"        bson:"views"`
-	Shares      int                  `json:"shares"       bson:"shares"`
-	SharedBy    []User               `json:"sharedBy"     bson:"shared_by"`
-	Tags        []string             `json:"tags"         bson:"tags"`
-	UpvotedBy   []primitive.ObjectID `json:"-"            bson:"upvoted_by"`
-	DownvotedBy []primitive.ObjectID `json:"-"            bson:"downvoted_by"`
-	PostType    string               `json:"postType"     bson:"post_type"`
-	Topics      []Topic              `json:"topics"       bson:"topics"`
-	Followers   []primitive.ObjectID `json:"-"            bson:"followers"`
-	Author      *User                `json:"author"       bson:"author"`
-	Answers     []primitive.ObjectID `json:"-"            bson:"answers"`
-	Answered    bool                 `json:"answered"     bson:"answered"`
-	ResponseTo  primitive.ObjectID   `json:"-"            bson:"response_to"`
-	Comments    []Comment            `json:"comments"     bson:"comments"`
-	CreatedAt   time.Time            `json:"createdAt"    bson:"created_at"`
-	UpdatedAt   time.Time            `json:"updatedAt"    bson:"updated_at"`
-	DeletedAt   time.Time            `json:"deletedAt"    bson:"deleted_at"`
+	ID          primitive.ObjectID   `json:"id,omitempty"           bson:"_id,omitempty"`
+	Title       string               `json:"title,omitempty"        bson:"title"`
+	Content     string               `json:"content,omitempty"      bson:"content"`
+	ContextLink string               `json:"ContextLink,omitempty"  bson:"context_link"`
+	Upvotes     int                  `json:"upvotes,omitempty"      bson:"upvotes"`
+	Downvotes   int                  `json:"downvotes,omitempty"    bson:"downvotes"`
+	Views       int                  `json:"views,omitempty"        bson:"views"`
+	Shares      int                  `json:"shares,omitempty"       bson:"shares"`
+	SharedBy    []primitive.ObjectID `json:"-"                      bson:"shared_by"`
+	UpvotedBy   []primitive.ObjectID `json:"-"                      bson:"upvoted_by"`
+	DownvotedBy []primitive.ObjectID `json:"-"                      bson:"downvoted_by"`
+	PostType    string               `json:"postType,omitempty"     bson:"post_type"`
+	Topics      []primitive.ObjectID `json:"topics,omitempty"       bson:"topics"`
+	Followers   []primitive.ObjectID `json:"-"                      bson:"followers"`
+	Author      *User                `json:"author,omitempty"       bson:"author"`
+	Answers     []primitive.ObjectID `json:"-"                      bson:"answers"`
+	ResponseTo  primitive.ObjectID   `json:"-"                      bson:"response_to"`
+	Comments    []primitive.ObjectID `json:"-"                      bson:"comments"`
+	CreatedAt   time.Time            `json:"createdAt"              bson:"created_at"`
+	UpdatedAt   time.Time            `json:"updatedAt"              bson:"updated_at"`
+	DeletedAt   time.Time            `json:"deletedAt"              bson:"deleted_at"`
 }
 
 // Validate - validates struct fields against defined rules
 func (p Post) Validate() error {
 	return validation.ValidateStruct(&p,
-		validation.Field(&p.Title, validation.Length(1, 250)),
-		validation.Field(&p.Content, validation.Required),
-		validation.Field(&p.PostType, validation.In([]interface{}{"post", "answer", "question"})),
+		validation.Field(&p.Title, validation.When(p.PostType != "post", validation.Required), validation.Length(1, 250)),
+		validation.Field(&p.Content, validation.When(p.PostType != "question", validation.Required)),
+		validation.Field(&p.PostType, validation.In("post", "answer", "question"), validation.Required),
 	)
 }
--- entity/user.entity.go
+++ entity/user.entity.go
@@ -12,30 +12,30 @@ import (
 
 // User - Defines the schema for a user collection
 type User struct {
-	ID           primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	Username     string               `json:"username"     bson:"username"`
-	Firstname    string               `json:"firstname"    bson:"firstname"`
-	Lastname     string               `json:"lastname"     bson:"lastname"`
-	Email        string               `json:"email"        bson:"email"`
-	Hash         string               `json:"hash"         bson:"hash"`
-	About        string               `json:"about"        bson:"about"`
-	Credential   string               `json:"credential"   bson:"credential"`
-	Avatar       string               `json:"avatar"       bson:"avatar"`
-	Views        int                  `json:"views"        bson:"views"`
-	Shares       []primitive.ObjectID `json:"-"            bson:"shares"`
-	Upvotes      int                  `json:"upvotes"      bson:"upvotes"`
-	Downvotes    int                  `json:"downvotes"    bson:"downvotes"`
-	Followers    []primitive.ObjectID `json:"-"            bson:"followers"`
-	Following    []primitive.ObjectID `json:"-"            bson:"following"`
-	Answers      []primitive.ObjectID `json:"-"            bson:"answers"`
-	Questions    []primitive.ObjectID `json:"-"            bson:"questions"`
-	Posts        []primitive.ObjectID `json:"-"            bson:"posts"`
-	Knowledge    []Topic              `json:"knowledge"    bson:"knowledge"`
-	Spaces       []Space              `json:"spaces"       bson:"spaces"`
-	PinnedAnswer Post                 `json:"pinnedAnswer" bson:"pinned_answer"`
-	CreatedAt    time.Time            `json:"createdAt"    bson:"created_at"`
-	UpdatedAt    time.Time            `json:"updatedAt"    bson:"updated_at"`
-	DeletedAt    time.Time            `json:"deletedAt"    bson:"deleted_at"`
+	ID         primitive.ObjectID   `json:"id,omitempty"           bson:"_id,omitempty"`
+	Username   string               `json:"username,omitempty"     bson:"username"`
+	Firstname  string               `json:"firstname,omitempty"    bson:"firstname"`
+	Lastname   string               `json:"lastname,omitempty"     bson:"lastname"`
+	Email      string               `json:"email,omitempty"        bson:"email"`
+	Hash       string               `json:"hash,omitempty"         bson:"hash"`
+	About      string               `json:"about,omitempty"        bson:"about"`
+	Credential string               `json:"credential,omitempty"   bson:"credential"`
+	Avatar     string               `json:"avatar,omitempty"       bson:"avatar"`
+	Views      int                  `json:"views,omitempty"        bson:"views"`
+	Shares     []primitive.ObjectID `json:"-"                      bson:"shares"`
+	Upvotes    int                  `json:"upvotes,omitempty"      bson:"upvotes"`
+	Downvotes  int                  `json:"downvotes,omitempty"    bson:"downvotes"`
+	Followers  []primitive.ObjectID `json:"-"                      bson:"followers"`
+	Following  []primitive.ObjectID `json:"-"                      bson:"following"`
+	Answers    []primitive.ObjectID `json:"-"                      bson:"answers"`
+	Questions  []primitive.ObjectID `json:"-"                      bson:"questions"`
+	Posts      []primitive.ObjectID `json:"-"                      bson:"posts"`
+	Knowledge  []Topic              `json:"knowledge,omitempty"    bson:"knowledge"`
+	Spaces     []Space              `json:"spaces,omitempty"       bson:"spaces"`
+	PinnedPost primitive.ObjectID   `json:"pinnedPost,omitempty"   bson:"pinned_post"`
+	CreatedAt  time.Time            `json:"createdAt"              bson:"created_at"`
+	UpdatedAt  time.Time            `json:"updatedAt"              bson:"updated_at"`
+	DeletedAt  time.Time            `json:"deletedAt"              bson:"deleted_at"`
 }
 
 // Validate - validates struct fields against defined rules
