f5f6a9f41f8aee69ecf6241aad4d05c4032a6103
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-22 15:48:12 -0600

    :sparkles: [feat]: define user-repository inteface methods

--- domain/user.domain.go
+++ domain/user.domain.go
@@ -2,7 +2,10 @@ package domain
 
 import (
 	"github.com/Mayowa-Ojo/kora/entity"
+	"github.com/Mayowa-Ojo/kora/types"
 	"github.com/gofiber/fiber"
+	mg "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // UserService -
@@ -13,4 +16,6 @@ type UserService interface {
 // UserRepository -
 type UserRepository interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.User, error)
+	GetOne(ctx *fiber.Ctx, filter types.Any, opts *options.FindOneOptions) (*entity.User, error)
+	Create(ctx *fiber.Ctx, user entity.User) (*mg.InsertOneResult, error)
 }
