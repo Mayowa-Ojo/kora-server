14c4740f3daeaa44c1e2bec31774fd372fa23731
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-22 15:49:58 -0600

    :recycle: [refactor]: define new 404 error message

--- middleware/error.go
+++ middleware/error.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber"
 // NotFoundError -
 func NotFoundError(c *fiber.Ctx) {
 	// send 404 error
-	err := fiber.NewError(404, "Resource not found.")
+	err := fiber.NewError(404, "Requested route doesn't exist.")
 
 	c.Next(err)
 }
