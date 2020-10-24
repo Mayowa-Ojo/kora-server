8666c3e4085206697ba1502efdaf1f36a6685cff
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-23 23:57:16 -0600

    :sparkles: [feat]: define topic-{repository,service} inteface methods

--- domain/topic.domain.go
+++ domain/topic.domain.go
@@ -2,15 +2,25 @@ package domain
 
 import (
 	"github.com/Mayowa-Ojo/kora/entity"
+	"github.com/Mayowa-Ojo/kora/types"
 	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // TopicService -
 type TopicService interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Topic, error)
+	GetOne(ctx *fiber.Ctx) (*entity.Topic, error)
+	Create(ctx *fiber.Ctx) (*entity.Topic, error)
 }
 
 // TopicRepository -
 type TopicRepository interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Topic, error)
+	GetOne(ctx *fiber.Ctx, filter types.Any) (*entity.Topic, error)
+	Create(ctx *fiber.Ctx, topic *entity.Topic) (*mongo.InsertOneResult, error)
+	GetMany(ctx *fiber.Ctx, filter types.Any, opts *options.FindOptions) ([]entity.Topic, error)
+	UpdateOne(ctx *fiber.Ctx, filter, update types.Any) (*mongo.UpdateResult, error)
+	DeleteOne(ctx *fiber.Ctx, filter types.Any) (*mongo.DeleteResult, error)
 }
