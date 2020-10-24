a329329d0ee42b2cbcc572cc9a21f1d927d1946c
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-10-23 23:55:10 -0600

    :sparkles: [feat]: implement service layer for topics

---
+++ services/topic.service.go
@@ -0,0 +1,85 @@
+package services
+
+import (
+	"github.com/Mayowa-Ojo/kora/constants"
+	"github.com/Mayowa-Ojo/kora/domain"
+	"github.com/Mayowa-Ojo/kora/entity"
+	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// TopicService -
+type TopicService struct {
+	topicRepo domain.TopicRepository
+}
+
+// NewTopicService -
+func NewTopicService(t domain.TopicRepository) domain.TopicService {
+	return &TopicService{
+		t,
+	}
+}
+
+// GetAll -
+func (t *TopicService) GetAll(ctx *fiber.Ctx) ([]entity.Topic, error) {
+	result, err := t.topicRepo.GetAll(ctx)
+
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	return result, nil
+}
+
+// GetOne -
+func (t *TopicService) GetOne(ctx *fiber.Ctx) (*entity.Topic, error) {
+	id := ctx.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	filter := bson.D{{Key: "_id", Value: objectID}}
+
+	topic, err := t.topicRepo.GetOne(ctx, filter)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, constants.ErrNotFound
+		}
+
+		return nil, constants.ErrInternalServer
+	}
+
+	return topic, nil
+}
+
+// Create -
+func (t *TopicService) Create(ctx *fiber.Ctx) (*entity.Topic, error) {
+	var requestBody struct {
+		Name string `json:"name"`
+	}
+
+	err := ctx.BodyParser(&requestBody)
+	if err != nil {
+		return nil, constants.ErrUnprocessableEntity
+	}
+
+	instance := &entity.Topic{
+		Name: requestBody.Name,
+	}
+
+	if err := instance.Validate(); err != nil {
+		return nil, constants.ErrUnprocessableEntity
+	}
+
+	insertResult, err := t.topicRepo.Create(ctx, instance)
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	filter := bson.D{{Key: "_id", Value: insertResult.InsertedID}}
+	topic, err := t.topicRepo.GetOne(ctx, filter)
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	return topic, nil
+}
