fd6d32415f8e2f0f83377e15d134b8e431fe7c10
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-04 13:03:01 -0700

    :sparkles: [feat]: add new env variables

---
+++ config/env.config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+// EnvConfig -
+type EnvConfig struct {
+	DBName             string `mapstructure:"DB_NAME"`
+	Port               int    `mapstructure:"PORT"`
+	JwtSecret          string `mapstructure:"JWT_SECRET"`
+	AwsAccessKeyID     string `mapstructure:"AWS_ACCESS_KEY_ID"`
+	AwsSecretAccessKey string `mapstructure:"AWS_SECRET_ACCESS_KEY"`
+	AwsRegion          string `mapstructure:"AWS_REGION"`
+	AwsS3Bucket        string `mapstructure:"AWS_S3_BUCKET"`
+}
+
+// NewEnvConfig -
+func NewEnvConfig() *EnvConfig {
+	var env *EnvConfig
+	v := viper.New()
+
+	v.SetConfigFile(".env")
+	v.SetConfigType("env")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("[Error]: could not read env file. %s", err)
+	}
+
+	if err := v.Unmarshal(&env); err != nil {
+		fmt.Printf("[Error]: could not decode env variables. %s", err)
+	}
+
+	return env
+}
