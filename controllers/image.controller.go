133a28960c1b65c2c716c80584f9742c7dfe79b6
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-04 13:05:34 -0700

    :sparkles: [feat]: implement presenter layer for images

---
+++ controllers/image.controller.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"github.com/Mayowa-Ojo/kora/domain"
+	"github.com/Mayowa-Ojo/kora/utils"
+	"github.com/gofiber/fiber"
+)
+
+// ImageController -
+type ImageController struct {
+	service domain.ImageService
+}
+
+// NewImageController -
+func NewImageController(i domain.ImageService) *ImageController {
+	return &ImageController{
+		i,
+	}
+}
+
+// UploadImage -
+func (i *ImageController) UploadImage(ctx *fiber.Ctx) {
+	result, err := i.service.UploadImage(ctx)
+	if err != nil {
+		ctx.Next(err)
+
+		return
+	}
+
+	r := utils.NewResponse()
+	r.JSONResponse(ctx, true, fiber.StatusCreated, "[INFO]: Resource created", result)
+}
