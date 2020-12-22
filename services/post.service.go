bc3b1f63e2e9e966b1ad8ff2daefc48f441808e5
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-21 21:28:12 -0700

    :sparkles: [feat]: implement create shared post method

--- services/post.service.go
+++ services/post.service.go
@@ -6,6 +6,8 @@ import (
 	"github.com/Mayowa-Ojo/kora/constants"
 	"github.com/Mayowa-Ojo/kora/domain"
 	"github.com/Mayowa-Ojo/kora/entity"
+	"github.com/Mayowa-Ojo/kora/shorturl"
+	"github.com/Mayowa-Ojo/kora/types"
 	"github.com/Mayowa-Ojo/kora/utils"
 	"github.com/gofiber/fiber"
 	"go.mongodb.org/mongo-driver/bson"
@@ -16,15 +18,21 @@ import (
 
 // PostService - acts as the business logic layer/service for posts
 type PostService struct {
-	postRepo domain.PostRepository
-	userRepo domain.UserRepository
+	postRepo       domain.PostRepository
+	userRepo       domain.UserRepository
+	sharedPostRepo domain.SharedPostRepository
+	commentRepo    domain.CommentRepository
 }
 
 // NewPostService - creates a new post service instance
-func NewPostService(p domain.PostRepository, u domain.UserRepository) domain.PostService {
+func NewPostService(
+	p domain.PostRepository, u domain.UserRepository, s domain.SharedPostRepository, c domain.CommentRepository,
+) domain.PostService {
 	return &PostService{
 		p,
 		u,
+		s,
+		c,
 	}
 }
 
@@ -67,21 +75,69 @@ func (p PostService) GetOne(ctx *fiber.Ctx) (*entity.Post, error) {
 	return post, nil
 }
 
+// GetBySlug -
+func (p PostService) GetBySlug(ctx *fiber.Ctx) (*entity.Post, error) {
+	slug := ctx.Query("slug")
+	username := ctx.Query("username")
+
+	filter := bson.D{
+		{Key: "slug", Value: slug},
+		{Key: "post_type", Value: "question"},
+	}
+	opts := options.FindOne()
+	opts.SetProjection(bson.D{{Key: "author.hash", Value: 0}})
+
+	post, err := p.postRepo.GetOne(ctx, filter, opts)
+	if err != nil {
+		if err == mg.ErrNoDocuments {
+			return nil, constants.ErrNotFound
+		}
+
+		return nil, constants.ErrInternalServer
+	}
+
+	if username != "" {
+		filter = bson.D{
+			{Key: "response_to", Value: post.ID},
+			{Key: "author.username", Value: username},
+		}
+		opts = options.FindOne()
+		opts.SetProjection(bson.D{{Key: "author.hash", Value: 0}})
+
+		post, err = p.postRepo.GetOne(ctx, filter, opts)
+		if err != nil {
+			println(err)
+			if err == mg.ErrNoDocuments {
+				return nil, constants.ErrNotFound
+			}
+
+			return nil, constants.ErrInternalServer
+		}
+	}
+
+	return post, nil
+}
+
 // Create -
 func (p PostService) Create(ctx *fiber.Ctx) (*entity.Post, error) {
 	var requestBody struct {
-		Title       string `json:"title"`
-		Content     string `json:"content"`
-		ContextLink string `json:"contextLink"`
-		PostType    string `json:"postType"`
+		Title            string `json:"title"`
+		Content          string `json:"content"`
+		ContentTruncated string `json:"contentTruncated"`
+		ContextLink      string `json:"contextLink"`
+		PostType         string `json:"postType"`
 	}
 
 	err := ctx.BodyParser(&requestBody)
 	if err != nil {
 		return nil, constants.ErrUnprocessableEntity
 	}
+
+	slug := utils.GenerateSlug(requestBody.Title)
+
 	instance := &entity.Post{
 		PostType: requestBody.PostType,
+		Slug:     slug,
 	}
 
 	switch requestBody.PostType {
@@ -91,17 +147,19 @@ func (p PostService) Create(ctx *fiber.Ctx) (*entity.Post, error) {
 			instance.ContextLink = requestBody.ContextLink
 		}
 	case "answer":
-		questionID := ctx.Query("question")
-		objectID, err := primitive.ObjectIDFromHex(questionID)
+		questionID := ctx.Query("questionId")
+		questionObjectID, err := primitive.ObjectIDFromHex(questionID)
 		if err != nil {
 			return nil, constants.ErrUnprocessableEntity
 		}
 
-		instance.ResponseTo = objectID
+		instance.ResponseTo = questionObjectID
 		instance.Title = requestBody.Title
 		instance.Content = requestBody.Content
+		instance.ContentTruncated = requestBody.ContentTruncated
 	case "post":
 		instance.Content = requestBody.Content
+		instance.ContentTruncated = requestBody.ContentTruncated
 	}
 
 	instance.SetDefaultValues()
@@ -117,7 +175,18 @@ func (p PostService) Create(ctx *fiber.Ctx) (*entity.Post, error) {
 		return nil, err
 	}
 
+	url, err := shorturl.CreateURL(ctx, types.GenericMap{
+		"slug":     slug,
+		"postType": requestBody.PostType,
+		"username": user.Username,
+	})
+
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
 	instance.Author = user
+	instance.ShareLink = url.ShortURL
 
 	insertResult, err := p.postRepo.Create(ctx, instance)
 	if err != nil {
@@ -133,6 +202,60 @@ func (p PostService) Create(ctx *fiber.Ctx) (*entity.Post, error) {
 	}
 
 	return post, nil
+}
+
+// CreateSharedPost - create a shared post
+func (p PostService) CreateSharedPost(ctx *fiber.Ctx) (*entity.SharedPost, error) {
+	postID := ctx.Params("id")
+	postObjectID, err := primitive.ObjectIDFromHex(postID)
+	if err != nil {
+		return nil, constants.ErrUnprocessableEntity
+	}
+
+	var requestBody struct {
+		ShareComment string `json:"shareComment"`
+	}
+
+	err = ctx.BodyParser(&requestBody)
+	if err != nil {
+		return nil, constants.ErrUnprocessableEntity
+	}
+
+	filter := bson.D{{Key: "_id", Value: postObjectID}}
+	opts := options.FindOne()
+	post, err := p.postRepo.GetOne(ctx, filter, opts)
+	if err != nil {
+		if err == mg.ErrNoDocuments || err == mg.ErrNilDocument {
+			return nil, constants.ErrNotFound
+		}
+		return nil, constants.ErrInternalServer
+	}
+
+	// fetch author for post
+	user, err := utils.GetUserFromAuthHeader(ctx, p.userRepo)
+	if err != nil {
+		return nil, err
+	}
+
+	instance := &entity.SharedPost{
+		Comment: requestBody.ShareComment,
+		Author:  user,
+		Post:    post,
+	}
+
+	insertResult, err := p.sharedPostRepo.Create(ctx, instance)
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	filter = bson.D{{Key: "_id", Value: insertResult.InsertedID}}
+	opts = options.FindOne()
+	sharedPost, err := p.sharedPostRepo.GetOne(ctx, filter, opts)
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	return sharedPost, nil
 }
 
 // DeleteOne - delete a single post
@@ -171,6 +294,22 @@ func (p PostService) GetFeedForUser(ctx *fiber.Ctx) ([]entity.Post, error) {
 			bson.D{{Key: "author._id", Value: bson.D{{Key: "$in", Value: user.Following}}}},
 			bson.D{{Key: "space._id", Value: bson.D{{Key: "$in", Value: user.Spaces}}}},
 		},
+	}}
+	postOpts := options.Find()
+	posts, err := p.postRepo.GetMany(ctx, filter, postOpts)
+
+	if err != nil {
+		return nil, constants.ErrInternalServer
+	}
+
+	return posts, nil
+}
+
+// GetQuestionsForUser - fetch questions for current user:
+func (p PostService) GetQuestionsForUser(ctx *fiber.Ctx) ([]entity.Post, error) {
+	filter := bson.D{{
+		Key:   "post_type",
+		Value: "question",
 	}}
 	postOpts := options.Find()
 	posts, err := p.postRepo.GetMany(ctx, filter, postOpts)
