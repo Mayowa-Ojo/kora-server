86ff66ecff322d7da6352785d92686c1125df971
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-29 20:07:41 -0700

    :sparkles: [feat]: register interface methods for comment

--- domain/comment.domain.go
+++ domain/comment.domain.go
@@ -13,13 +13,16 @@ type CommentService interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Comment, error)
 	GetOne(ctx *fiber.Ctx) (*entity.Comment, error)
 	Create(ctx *fiber.Ctx) (*entity.Comment, error)
+	CreateCommentReply(ctx *fiber.Ctx) (*entity.Comment, error)
 	GetCommentsForPost(ctx *fiber.Ctx) ([]entity.Comment, error)
+	AppendCommentsToPost(ctx *fiber.Ctx, post *entity.Post) (*entity.Post, error)
+	AppendCommentsToPosts(ctx *fiber.Ctx, posts []entity.Post) ([]entity.Post, error)
 }
 
 // CommentRepository -
 type CommentRepository interface {
 	GetAll(ctx *fiber.Ctx) ([]entity.Comment, error)
-	GetOne(ctx *fiber.Ctx, filter types.Any) (*entity.Comment, error)
+	GetOne(ctx *fiber.Ctx, filter types.Any, opts *options.FindOneOptions) (*entity.Comment, error)
 	Create(ctx *fiber.Ctx, comment *entity.Comment) (*mongo.InsertOneResult, error)
 	GetMany(ctx *fiber.Ctx, filter types.Any, opts *options.FindOptions) ([]entity.Comment, error)
 	UpdateOne(ctx *fiber.Ctx, filter, update types.Any) (*mongo.UpdateResult, error)
